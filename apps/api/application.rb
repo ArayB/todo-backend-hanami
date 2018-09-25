197e53fa2244cfb5e3bc6a4cc3e743f90d714c2f
Ant Barnes <anthony.barnes@thefloow.com>
2018-09-25 12:30:52 +0100

    Add host for production urls

--- apps/api/application.rb
+++ apps/api/application.rb
@@ -47,6 +47,9 @@ module Api
       # Defaults to "localhost"
       #
       # host 'example.org'
+      if Hanami.env?(:production)
+        host 'todo-backend-hanami.herokuapp.com'
+      end
 
       # URI port used by the routing system to generate absolute URLs
       # Argument: An object coercible to integer, defaults to 80 if the scheme
