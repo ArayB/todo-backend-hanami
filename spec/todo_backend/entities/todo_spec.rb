488be4ca62412423c694ef6127db0cdb7993803d
Ant Barnes <anthony.barnes@thefloow.com>
2018-09-28 14:14:38 +0100

    Add test for #url on todo entity

--- spec/todo_backend/entities/todo_spec.rb
+++ spec/todo_backend/entities/todo_spec.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
 RSpec.describe Todo, type: :entity do
-  # place your tests here
+  describe '#url' do
+    it 'returns correct url' do
+      todo = TodoRepository.new.create(title: 'Title')
+      # Domain is set in .env.test
+      expect(todo.url).to eq("https://example.com#{Api.routes.path(:todo, todo.id)}")
+    end
+  end
 end
